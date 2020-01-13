7dcf74ef94fdfaba3c94dd692d01dd05dfb7b780
Maria Eugênia <mariaeugenia.dev@gmail.com>
2020-01-12 21:46:43 -0300

    added http; perfome a request

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'strings.dart';
 
 void main() => runApp(MyApp());
@@ -14,17 +16,33 @@ class MyApp extends StatelessWidget {
 }
 
 class Weather extends StatefulWidget {
+
+  //final _biggerFont =  const  TextStyle(fontSize: 18.0);
+
   @override
   createState() => WeatherState();
 }
 
 class WeatherState extends State<Weather> {
+
+  _loadData() async {
+  String dataURL = Strings.baseURL+"London&APPID="+Strings.apiKey;
+  http.Response response = await http.get(dataURL);
+  setState(() {
+    var data = json.decode(response.body);
+    print(data);
+  });
+}
+
+@override
+void initState() {
+  super.initState();
+
+  _loadData();
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold (
-      appBar: AppBar(
-        title: Text(Strings.appTitle),
-      ),
       body: Text(Strings.appTitle),
     );
   }
--- lib/strings.dart
+++ lib/strings.dart
@@ -1,3 +1,5 @@
 class Strings {
+  static String apiKey = "5e48ae5417f30cad45bdf45b31c06371";
+  static String baseURL = "https://api.openweathermap.org/data/2.5/weather?q=";
   static String appTitle = "Wheather";
 }
